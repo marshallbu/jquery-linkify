49008527fd7573d68cb9adcb14e6040501fc1cfd
Marshall Upshur <marshall@mbucreations.com>
2013-03-26 20:19:50 -0700

    Putting everything in an options object
    putting everything in an options object so I can pass in more custom
    functions if needs be.

--- jquery.linkify.js
+++ jquery.linkify.js
@@ -4,7 +4,7 @@
  * https://github.com/marshallbu/jquery-linkify
  *
  */
-function linkify(string, buildHashtagUrl, options) {
+function linkify(string, options) {
   var urlRegEx = /(?:(https?|ftps?|file):\/\/)?[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(?:\/?[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
   var emailRegEx = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4})$/i;
 
@@ -34,8 +34,12 @@ function linkify(string, buildHashtagUrl, options) {
     return '<a href="' + uri + '" target="' + options.target + '">' + captured + '</a>';
   });
 
-  if (buildHashtagUrl) {
-    string = string.replace(/\B#(\w+)/g, '<a href="' + buildHashtagUrl('$1') + '" target="' + options.target + '">#$1</a>');
+  if (options.buildHashtagUrl) {
+    string = string.replace(/\B#(\w+)/g, '<a href="' + options.buildHashtagUrl('$1') + '" target="' + options.target + '">#$1</a>');
+  }
+
+  if (options.buildMailToUrl) {
+    string = string.replace(/\B#(\w+)/g, '<a href="' + options.buildMailToUrl('$1') + '" target="' + options.target + '">#$1</a>');
   }
 
   return string;
@@ -45,20 +49,26 @@ function linkify(string, buildHashtagUrl, options) {
   $.fn.linkify = function(opts) {
     return this.each(function() {
       var $this = $(this);
-      var buildHashtagUrl;
       var options = {
         includeW3: true,
         target: '_self',
         alwaysIncludeProtocol: false,
-        defaultProtocol: 'http://'
+        defaultProtocol: 'http://',
+        buildHashtagUrl: null,
+        buildMailToUrl: null
       };
 
       if (opts) {
         if (typeof opts  === 'function') {
-          buildHashtagUrl = opts;
+          // TODO: allow for just a function to be passed in if you want to go with defaults
+          // since there are 2 (and will be more) function options, this should be interesting
+          // buildHashtagUrl = opts;
         } else {
           if (typeof opts.hashtagUrlBuilder === 'function') {
-            buildHashtagUrl = opts.hashtagUrlBuilder;
+            options.buildHashtagUrl = opts.hashtagUrlBuilder;
+          }
+          if (typeof opts.mailToUrlBuilder === 'function') {
+            options.buildMailToUrl = opts.mailToUrlBuilder;
           }
           if (typeof opts.includeW3 === 'boolean') {
             options.includeW3 = opts.includeW3;
@@ -72,7 +82,7 @@ function linkify(string, buildHashtagUrl, options) {
         }
       }
 
-      $this.html(linkify($this.html(), buildHashtagUrl, options));
+      $this.html(linkify($this.html(), options));
     });
   };
 
